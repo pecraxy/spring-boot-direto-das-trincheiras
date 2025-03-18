0ed0c6bda3fc588b2cf4d93cc55b8ba55476b0a7
pecraxy <bemerda@hotmail.com>
2025-03-17 22:58:15 -0300

    video07: partial commit

--- anime-service/src/main/java/academy/devdojo/controller/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controller/HelloController.java
@@ -2,10 +2,13 @@ package academy.devdojo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+@RequestMapping("greetings")
 public class HelloController {
-    @GetMapping("hi")
+    @GetMapping("/hi")
     public ResponseEntity<String> hi(){
         return ResponseEntity.ok().body("OMAE WA MOU SHINDEIRU");
     }
