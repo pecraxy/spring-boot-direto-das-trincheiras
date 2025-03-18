56ab854e3614222984b72aa13c0806e31b31eed8
pecraxy <bemerda@hotmail.com>
2025-03-18 16:59:19 -0300

    video09: did the exercise 01

--- anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
+++ anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
@@ -1,20 +1,16 @@
 package academy.devdojo;
 
-import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
 @SpringBootApplication
 @Log4j2
-@ComponentScan(basePackages = {"outside.devdojo", "academy.devdojo"})
 public class AnimeServiceApplication {
-	public static void main(String[] args) {
-		var applicationContext = SpringApplication.run(AnimeServiceApplication.class, args);
-		Arrays.stream(applicationContext.getBeanDefinitionNames()).forEach(log::info);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeServiceApplication.class, args);
+    }
 }
