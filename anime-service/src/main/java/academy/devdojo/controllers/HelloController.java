30fab7aed891f8b4f02747959df02f8fa8291b60
pecraxy <bemerda@hotmail.com>
2025-03-17 21:17:57 -0300

    video06: created a controller with a get mapping

--- anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
@@ -1,12 +1,13 @@
 package academy.devdojo.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("")
-    public String hi(){
-        return "OMAE WA MOU SHINDE IRU";
+    @GetMapping("hi")
+    public ResponseEntity<String> hi(){
+        return ResponseEntity.ok().body("OMAE WA MOU SHINDE IRU");
     }
 }
