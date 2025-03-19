acba9b145d1efb96d90c0e6fb701572053b1221b
pecraxy <bemerda@hotmail.com>
2025-03-19 10:28:10 -0300

    video10: learned about virtual threads and learned how to omit the exception stracktace

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -1,19 +1,20 @@
 package academy.devdojo.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("v1/animes")
 @Slf4j
 public class AnimeController {
     @GetMapping
-    public List<String> listAll(){
+    public List<String> listAll() throws InterruptedException {
         log.info(Thread.currentThread().getName());
+        TimeUnit.SECONDS.sleep(1);
         return List.of("Tokyo Ghoul", "Dungeon nii deai", "Kill la Kill", "Saiki Kusuo no Psi Nan");
     }
 }
