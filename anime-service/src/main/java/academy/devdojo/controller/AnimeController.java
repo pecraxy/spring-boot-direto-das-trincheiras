ca7e8bde62f63dd88d07e042a034b7cb78a14623
pecraxy <bemerda@hotmail.com>
2025-03-18 22:32:45 -0300

    video10: partial commit

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -1,5 +1,6 @@
 package academy.devdojo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("v1/animes")
+@Slf4j
 public class AnimeController {
-    private List<String> animes = List.of("Tokyo Ghoul", "Dungeon nii deai", "Kill la Kill", "Saiki Kusuo no Psi Nan");
     @GetMapping
-    public ResponseEntity<List<String>> listAll(){
-        return ResponseEntity.ok().body(animes);
+    public List<String> listAll(){
+        log.info(Thread.currentThread().getName());
+        return List.of("Tokyo Ghoul", "Dungeon nii deai", "Kill la Kill", "Saiki Kusuo no Psi Nan");
     }
 }
