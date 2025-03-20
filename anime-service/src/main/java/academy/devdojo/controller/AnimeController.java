dc7681a46057e721e9dd6bff8f6ef103f42b38ae
pecraxy <bemerda@hotmail.com>
2025-03-20 10:23:25 -0300

    video15: exercise03 - postmapping

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -3,27 +3,33 @@ package academy.devdojo.controller;
 import academy.devdojo.domain.Anime;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping("v1/animes")
 @Slf4j
 public class AnimeController {
+
+
     @GetMapping
     public List<Anime> listAll(@RequestParam(required = false) String name){
-        List<Anime> animes = Anime.getAnimes();
-        if (name == null) return animes;
+        if (name == null) return Anime.getAnimes();
         return Anime.getAnimes().stream()
                 .filter(anime -> anime.getName().contains(name))
                 .toList();
     }
 
+    @PostMapping
+    public Anime save(@RequestBody Anime anime){
+        anime.setId(ThreadLocalRandom.current().nextLong(1, 100));
+        Anime.getAnimes().add(anime);
+        return anime;
+    }
 
     @GetMapping("{id}")
     public Anime filterById(@PathVariable Long id){
-        List<Anime> animeList = Anime.getAnimes();
-        return animeList.stream()
+        return Anime.getAnimes().stream()
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElse(null);
--- anime-service/src/main/java/academy/devdojo/domain/Anime.java
+++ anime-service/src/main/java/academy/devdojo/domain/Anime.java
@@ -1,21 +1,30 @@
 package academy.devdojo.domain;
 
-import lombok.Builder;
-import lombok.Value;
+import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Value
+@Getter
+@Setter
 @Builder
+@AllArgsConstructor
 public class Anime {
     private Long id;
     private String name;
-
-    public static List<Anime> getAnimes(){
+    private static List<Anime> animeList = new ArrayList<>();
+    static {
         Anime anime1 = Anime.builder().id(1L).name("Saiki Kusuo no Psi Nan").build();
         Anime anime2 = Anime.builder().id(2L).name("Dragon Ball Z").build();
         Anime anime3 = Anime.builder().id(3L).name("Sword Art Online").build();
         Anime anime4 = Anime.builder().id(4L).name("Shangri-la Frontiers").build();
-        return List.of(anime1, anime2, anime3, anime4);
+        animeList.add(anime1);
+        animeList.add(anime2);
+        animeList.add(anime3);
+        animeList.add(anime4);
+    }
+
+    public static List<Anime> getAnimes(){
+        return animeList;
     }
 }
