2f1fdaad3d16174de1b638ee5175539905bffb59
pecraxy <bemerda@hotmail.com>
2025-04-07 17:02:50 -0300

    video37: unit test - services pt 01

---
+++ anime-service/src/test/java/academy/devdojo/service/ProducerServiceTest.java
@@ -0,0 +1,78 @@
+package academy.devdojo.service;
+
+import academy.devdojo.domain.Producer;
+import academy.devdojo.repository.ProducerHardCodedRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ProducerServiceTest {
+    @InjectMocks
+    private ProducerService service;
+
+    @Mock
+    private ProducerHardCodedRepository repository;
+
+    private List<Producer> producerList;
+
+    @BeforeEach
+    void init() {
+        Producer producer1 = Producer.builder().id(1L).name("Ufotable").createdAt(LocalDateTime.now()).build();
+        Producer producer2 = Producer.builder().id(2L).name("Wit Studio").createdAt(LocalDateTime.now()).build();
+        Producer producer3 = Producer.builder().id(3L).name("Studios Ghibli").createdAt(LocalDateTime.now()).build();
+        producerList = new ArrayList<>(List.of(producer1, producer2, producer3));
+    }
+
+    @Test
+    @DisplayName("findAll returns a list with all producers when argument is null")
+    @Order(1)
+    void findAll_ReturnAllProducers_WhenArgumentIsNull() {
+        BDDMockito.when(repository.findAll()).thenReturn(producerList);
+        var producers = service.findAll(null);
+        Assertions.assertThat(producers)
+                .hasSize(producerList.size())
+                .hasSameElementsAs(producerList);
+    }
+
+    @Test
+    @DisplayName("findAll returns a list with found object when name exists")
+    @Order(2)
+    void findAll_ReturnFoundProducer_WhenNameExists() {
+        var producer = producerList.getFirst();
+
+        List<Producer> expectedProducersFound = Collections.singletonList(producer);
+
+        BDDMockito.when(repository.findByName(producer.getName()))
+                .thenReturn(expectedProducersFound);
+
+        List<Producer> producersFound = service.findAll(producer.getName());
+
+        Assertions.assertThat(producersFound)
+                .hasSize(expectedProducersFound.size())
+                .hasSameElementsAs(expectedProducersFound)
+                .containsAll(expectedProducersFound);
+    }
+
+    @Test
+    @DisplayName("findAll returns empty list when name is not found")
+    @Order(3)
+    void findAll_ReturnsEmptyList_WhenNameIsNotFound() {
+        var name = "not-found";
+        BDDMockito.when(repository.findByName(name)).thenReturn(Collections.emptyList());
+        List<Producer> foundProducers = service.findAll(name);
+        Assertions.assertThat(foundProducers)
+                .isNotNull()
+                .isEmpty();
+    }
+}
