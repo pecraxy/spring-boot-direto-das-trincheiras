c07a88ecdd8233b4dcc153ffdb790002be897518
pecraxy <bemerda@hotmail.com>
2025-04-10 17:57:59 -0300

    video42:

--- anime-service/src/test/java/academy/devdojo/controller/ProducerControllerTest.java
+++ anime-service/src/test/java/academy/devdojo/controller/ProducerControllerTest.java
@@ -1,29 +1,28 @@
 package academy.devdojo.controller;
 
 import academy.devdojo.domain.Producer;
-import academy.devdojo.mapper.ProducerMapperImpl;
 import academy.devdojo.repository.ProducerData;
-import academy.devdojo.repository.ProducerHardCodedRepository;
-import academy.devdojo.service.ProducerService;
 import org.junit.jupiter.api.*;
 import org.mockito.BDDMockito;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 @WebMvcTest(controllers = ProducerController.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Import({ProducerMapperImpl.class, ProducerService.class, ProducerHardCodedRepository.class, ProducerData.class})
+@ComponentScan(basePackages = "academy.devdojo")
 class ProducerControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -31,12 +30,17 @@ class ProducerControllerTest {
     @MockitoBean
     private ProducerData producerData;
     private List<Producer> producerList;
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @BeforeEach
     void init() {
-        Producer producer1 = Producer.builder().id(1L).name("Ufotable").createdAt(LocalDateTime.now()).build();
-        Producer producer2 = Producer.builder().id(2L).name("Wit Studio").createdAt(LocalDateTime.now()).build();
-        Producer producer3 = Producer.builder().id(3L).name("Studios Ghibli").createdAt(LocalDateTime.now()).build();
+        String dateTime = "2025-04-10T16:38:33.2941297";
+        var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS");
+        LocalDateTime localDateTime = LocalDateTime.parse(dateTime, formatter);
+        Producer producer1 = Producer.builder().id(1L).name("Ufotable").createdAt(localDateTime).build();
+        Producer producer2 = Producer.builder().id(2L).name("Wit Studio").createdAt(localDateTime).build();
+        Producer producer3 = Producer.builder().id(3L).name("Studios Ghibli").createdAt(localDateTime).build();
         producerList = new ArrayList<>(List.of(producer1, producer2, producer3));
     }
 
@@ -45,10 +49,16 @@ class ProducerControllerTest {
     @Order(1)
     void findAll_ReturnAllProducers_WhenArgumentIsNull() throws Exception {
         BDDMockito.when(producerData.getProducers()).thenReturn(producerList);
+        var response = readResourceFile("producer/get-producer-null-name-200.json");
         mockMvc.perform(MockMvcRequestBuilders.get("/v1/producers"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].id").value(1L)); // Comparação de atributo, forma errada, visto que caso precise comparar o JSON inteiro, teria que adicionar vários valores
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    private String readResourceFile(String fileName) throws IOException {
+        var file = resourceLoader.getResource("classpath:%s".formatted(fileName)).getFile();
+        return new String(Files.readAllBytes(file.toPath()));
     }
 
 }
