fc094beba2bcf9830ad8074f3d8ce9371cf5fc4c
pecraxy <bemerda@hotmail.com>
2025-04-16 17:22:22 -0300

    partial commit

--- anime-service/src/main/java/academy/devdojo/config/ConnectionConfiguration.java
+++ anime-service/src/main/java/academy/devdojo/config/ConnectionConfiguration.java
@@ -1,14 +1,23 @@
 package academy.devdojo.config;
 
 import external.dependency.Connection;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConnectionConfiguration {
+
+    @Value("${database.mysql.url}")
+    private String url;
+    @Value("${database.mysql.username}")
+    private String username;
+    @Value("${database.mysql.password}")
+    private String password;
+
     @Bean
     public Connection connectionMySql(){
-        return new Connection("localhost", "devdojoMySql", "goku");
+        return new Connection(url, username, password);
     }
 
     @Bean
