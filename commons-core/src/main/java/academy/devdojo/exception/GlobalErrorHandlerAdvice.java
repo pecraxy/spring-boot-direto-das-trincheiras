3be8ef0f82021ad57c9962b561a3680514cb693b
pecraxy <bemerda@hotmail.com>
2025-11-21 20:58:54 -0300

    video78: treating  sqlintegrityconstraintviolation

--- commons-core/src/main/java/academy/devdojo/exception/GlobalErrorHandlerAdvice.java
+++ commons-core/src/main/java/academy/devdojo/exception/GlobalErrorHandlerAdvice.java
@@ -5,11 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 @RestControllerAdvice
 public class GlobalErrorHandlerAdvice {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<DefaultErrorMessage> handleNotFoundException(NotFoundException e){
         DefaultErrorMessage error = new DefaultErrorMessage(HttpStatus.NOT_FOUND.value(), e.getReason());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<DefaultErrorMessage> handleSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException e){
+        DefaultErrorMessage error = new DefaultErrorMessage(HttpStatus.BAD_REQUEST.value(), "Duplicated entry for one of the unique fields.");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
