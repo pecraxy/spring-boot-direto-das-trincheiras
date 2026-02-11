21dc63708669af02014ffd45c34520b829e3f7ee
pecraxy <bemerda@hotmail.com>
2026-02-11 14:28:50 -0300

    video88: @transactional

--- user-service/src/main/java/academy/devdojo/service/UserService.java
+++ user-service/src/main/java/academy/devdojo/service/UserService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -25,6 +26,7 @@ public class UserService {
         return repository.findById(id).orElseThrow(() -> new NotFoundException("User not found"));
     }
 
+//    @Transactional
     public User save(User user) {
         assertEmailDoesNotExists(user.getEmail());
         return repository.save(user);
