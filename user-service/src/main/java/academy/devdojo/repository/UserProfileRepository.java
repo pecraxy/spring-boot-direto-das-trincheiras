c79d1bc11640876e38979c5e8db43d8cd7553fae
pecraxy <bemerda@hotmail.com>
2026-02-18 14:25:51 -0300

    video97: queries with jpql

--- user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
+++ user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
@@ -2,7 +2,12 @@ package academy.devdojo.repository;
 
 import academy.devdojo.domain.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
 
+    @Query("SELECT up from UserProfile up join fetch up.user u join fetch up.profile p")
+    List<UserProfile> retrieveAll();
 }
