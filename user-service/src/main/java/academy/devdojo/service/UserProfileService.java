b4b64db6f31896b34f1542765818f6ca299a2b84
pecraxy <bemerda@hotmail.com>
2026-02-18 13:31:02 -0300

    video94: spring data jpa many to many pt 02

---
+++ user-service/src/main/java/academy/devdojo/service/UserProfileService.java
@@ -0,0 +1,20 @@
+package academy.devdojo.service;
+
+import academy.devdojo.domain.UserProfile;
+import academy.devdojo.repository.UserProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserProfileService {
+    private final UserProfileRepository repository;
+
+    public List<UserProfile> findAll() {
+        return repository.findAll();
+    }
+
+
+}
