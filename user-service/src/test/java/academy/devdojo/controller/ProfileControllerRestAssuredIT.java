987f25975a6ec10d7f08adaed1d21b98c58d183c
pecraxy <bemerda@hotmail.com>
2026-03-18 10:40:55 -0300

    video113: RestAssured pt 02

--- user-service/src/test/java/academy/devdojo/controller/ProfileControllerRestAssuredIT.java
+++ user-service/src/test/java/academy/devdojo/controller/ProfileControllerRestAssuredIT.java
@@ -4,6 +4,7 @@ import academy.devdojo.commons.FileUtils;
 import academy.devdojo.config.IntegrationTestConfig;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import net.javacrumbs.jsonunit.assertj.JsonAssertions;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.provider.Arguments;
@@ -53,29 +54,46 @@ class ProfileControllerRestAssuredIT extends IntegrationTestConfig {
                 .log().all();
     }
 
-//    @Test
-//    @DisplayName("GET /v1/profiles returns empty list when nothing is not found")
-//    @Order(2)
-//    void findAll_ReturnsEmptyList_WhenNothingIsNotFound() {
-//        var typeReference = new ParameterizedTypeReference<List<ProfileGetResponse>>() {};
-//        var responseEntity = testRestTemplate.exchange(URL, GET, null, typeReference);
-//        assertThat(responseEntity).isNotNull();
-//        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        assertThat(responseEntity.getBody()).isNotNull().isEmpty();
-//    }
-//
-//    @Test
-//    @Order(3)
-//    @DisplayName("POST v1/profiles save a Profile when successful")
-//    void save_SaveAProfile_WhenSuccessful() throws Exception {
-//        var request = fileUtils.readSourceFile("/profiles/post-request-profiles-201.json");
-//        var profileEntity = buildHttpEntity(request);
-//        var responseEntity = testRestTemplate.exchange(URL, POST, profileEntity, ProfilePostResponse.class);
-//
-//        assertThat(responseEntity).isNotNull();
-//        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-//        assertThat(responseEntity.getBody()).isNotNull().hasNoNullFieldsOrProperties();
-//    }
+    @Test
+    @DisplayName("GET /v1/profiles returns empty list when nothing is not found")
+    @Order(2)
+    void findAll_ReturnsEmptyList_WhenNothingIsNotFound() {
+        var response = fileUtils.readSourceFile("/profiles/get-profiles-empty-list-200.json");
+        RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .when()
+                .get(URL)
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body(Matchers.equalTo(response))
+                .log().all();
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("POST v1/profiles save a Profile when successful")
+    void save_SaveAProfile_WhenSuccessful() {
+        var request = fileUtils.readSourceFile("profiles/post-request-profiles-201.json");
+        var expectedResponse = fileUtils.readSourceFile("profiles/post-response-profiles-201.json");
+
+        String response = RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(URL)
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .log().all()
+                .extract().response().body().asString();
+
+        JsonAssertions.assertThatJson(response)
+                .node("id")
+                .asNumber()
+                .isPositive();
+        JsonAssertions.assertThatJson(response)
+                .whenIgnoringPaths("id")
+                .isEqualTo(expectedResponse);
+    }
 //
 //    @ParameterizedTest
 //    @MethodSource("postProfileBadRequestSource")
