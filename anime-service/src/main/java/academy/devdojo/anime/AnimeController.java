9af2dd88958e08b6bccfc8f48c08293a319adbc0
pecraxy <bemerda@hotmail.com>
2026-04-11 13:27:45 -0300

    minha solução do exercício

--- anime-service/src/main/java/academy/devdojo/anime/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/anime/AnimeController.java
@@ -1,10 +1,16 @@
 package academy.devdojo.anime;
 
 import academy.devdojo.domain.Anime;
+import academy.devdojo.exception.DefaultErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -25,6 +31,17 @@ public class AnimeController {
     private final AnimeService service;
 
     @GetMapping
+    @Operation(
+            summary = "Find all Animes in System",
+            responses = {
+                    @ApiResponse(description = "Get Animes",
+                            responseCode = "200",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                schema = @Schema(implementation = AnimeGetResponse.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<List<AnimeGetResponse>> findAll(@RequestParam(required = false) String name) {
 
         List<Anime> animes = service.findAll(name);
@@ -35,13 +52,29 @@ public class AnimeController {
     }
 
     @GetMapping("/paginated")
-    public ResponseEntity<Page<AnimeGetResponse>> findAllPaginated(Pageable pageable) {
+    @Operation(summary = "Get Paginated Animes",
+            responses = {
+                @ApiResponse(description = "Paginated Animes", responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Page.class)))
+            }
+    )
+    public ResponseEntity<Page<AnimeGetResponse>> findAllPaginated(@ParameterObject Pageable pageable) {
         log.debug("Request received to list all animes paginated");
         Page<AnimeGetResponse> pageAnime = service.findAllPaginated(pageable).map(mapper::toAnimeGetResponse);
         return ResponseEntity.ok(pageAnime);
     }
 
     @GetMapping("{id}")
+    @Operation(summary = "Get Anime by Id",
+            responses = {
+                    @ApiResponse(description = "Get Anime by its id",
+                            responseCode = "200",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AnimeGetResponse.class))
+                    ),
+                    @ApiResponse(description = "Anime Not Found",
+                            responseCode = "404",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DefaultErrorMessage.class))
+                    )
+            })
     public ResponseEntity<AnimeGetResponse> findById(@PathVariable Long id) {
         Anime foundAnime = service.findByIdOrThrowNotFound(id);
 
@@ -51,6 +84,19 @@ public class AnimeController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Creates new Anime in the System",
+            responses = {
+                    @ApiResponse(description = "Creates new Anime",
+                            responseCode = "201",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AnimePostResponse.class))
+                    ),
+                    @ApiResponse(description = "Validation Error",
+                            responseCode = "400",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DefaultErrorMessage.class)
+                            )
+                    ),
+            }
+    )
     public ResponseEntity<AnimePostResponse> save(@RequestBody @Valid AnimePostRequest request) {
         Anime anime = mapper.toAnime(request);
         Anime savedAnime = service.save(anime);
@@ -59,17 +105,46 @@ public class AnimeController {
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "Deletes an Anime in the System",
+            responses = {
+                    @ApiResponse(description = "Deletes an Anime",
+                            responseCode = "204",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+                    ),
+                    @ApiResponse(description = "Anime not found",
+                            responseCode = "404",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DefaultErrorMessage.class)
+                            )
+                    ),
+            }
+    )
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping
+    @Operation(summary = "Updates an Anime in the System",
+            responses = {
+                    @ApiResponse(description = "Updates an Anime",
+                            responseCode = "201",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AnimePostResponse.class))
+                    ),
+                    @ApiResponse(description = "Validation Error",
+                            responseCode = "400",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DefaultErrorMessage.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Anime not found",
+                            responseCode = "404",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DefaultErrorMessage.class)
+                            )
+                    ),
+            }
+    )
     public ResponseEntity<Void> update(@RequestBody @Valid AnimePutRequest request) {
         Anime animeToUpdate = mapper.toAnime(request);
         service.update(animeToUpdate);
         return ResponseEntity.noContent().build();
     }
-
-
 }
