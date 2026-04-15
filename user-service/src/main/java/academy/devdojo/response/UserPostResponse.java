7d750a9413dff37b78e69b41fc140307c0be387f
pecraxy <bemerda@hotmail.com>
2026-04-15 10:19:50 -0300

    finalizacao

--- user-service/src/main/java/academy/devdojo/response/UserPostResponse.java
+++ user-service/src/main/java/academy/devdojo/response/UserPostResponse.java
@@ -1,10 +1,12 @@
 package academy.devdojo.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class UserPostResponse {
+    @Schema(example = "99", description = "Unique Identifier created to new User")
     private Long id;
 }
